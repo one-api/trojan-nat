6e27e0b1238070506459b0cc6c564648c96f9cba
one-api <57555932+one-api@users.noreply.github.com>
2024-06-16 17:04:17 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,49 @@
+SERVER=$1
+PASSWORD=$2
+
+sudo sysctl -w net.ipv4.conf.all.forwarding=1
+
+sudo mkdir -p /etc/trojan-go
+
+# config file
+cat <<EOF >config.json
+{
+    "run_type": "nat",
+    "local_addr": "0.0.0.0",
+    "local_port": 1080,
+    "remote_addr": "$SERVER",
+    "remote_port": 443,
+    "password": [
+        "$PASSWORD"
+    ]
+}
+EOF
+sudo mv config.json /etc/trojan-go/config.json
+
+
+# service file
+cat <<EOF >trojan-go.service
+[Unit]
+After=network.target nss-lookup.target
+
+[Service]
+User=root
+CapabilityBoundingSet=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+AmbientCapabilities=CAP_NET_ADMIN CAP_NET_BIND_SERVICE
+NoNewPrivileges=true
+ExecStart=trojan-go -config /etc/trojan-go/config.json
+ExecStartPost=sh /etc/trojan-go/nat.sh
+Restart=on-failure
+RestartSec=3s
+LimitNOFILE=infinity
+
+[Install]
+WantedBy=multi-user.target    
+EOF
+
+sudo mv trojan-go.service /etc/systemd/system/
+
+
+# etc file
+mkdir -p /etc/trojan-go/
+cp nat.sh /etc/trojan-go/nat.sh
